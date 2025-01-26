6099bfc892383a5f3b7fe6c6d09a93f8a54b8b78
mr.jashwanthreddy <89520760+JashwanthReddy13189@users.noreply.github.com>
2025-01-26 22:54:49 +0530

    JPA Implementation

--- mj-ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ mj-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -1,7 +1,13 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
