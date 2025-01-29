87455631f528d5061b7123473829405ae54c36ac
mr.jashwanthreddy <89520760+JashwanthReddy13189@users.noreply.github.com>
2025-01-29 21:42:27 +0530

    Products

--- mj-ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ mj-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "categories")
 @Data
@@ -20,4 +22,7 @@ public class Category {
     @Size(min = 5, message = "Category name must contain atleast 5 characters")
     private String categoryName;
 
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
 }
