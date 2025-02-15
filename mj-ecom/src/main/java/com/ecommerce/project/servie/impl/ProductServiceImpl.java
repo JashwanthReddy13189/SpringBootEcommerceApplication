d20015f889c557a880f8d9551ec2a4bad47df643
mr.jashwanthreddy <89520760+JashwanthReddy13189@users.noreply.github.com>
2025-02-15 16:27:52 +0530

    JWT Authentication and Cookie Based Auth Implementation

--- mj-ecom/src/main/java/com/ecommerce/project/servie/impl/ProductServiceImpl.java
+++ mj-ecom/src/main/java/com/ecommerce/project/servie/impl/ProductServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import jakarta.servlet.http.Part;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
