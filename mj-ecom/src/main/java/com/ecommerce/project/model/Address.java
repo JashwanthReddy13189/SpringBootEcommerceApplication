dcbc18f91df82bb08aa533d0210c8d10f363f7c6
mr.jashwanthreddy <89520760+JashwanthReddy13189@users.noreply.github.com>
2025-02-16 14:42:05 +0530

    Users Address Management Module

--- mj-ecom/src/main/java/com/ecommerce/project/model/Address.java
+++ mj-ecom/src/main/java/com/ecommerce/project/model/Address.java
@@ -8,9 +8,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Table(name = "addresses")
 @Data
@@ -47,9 +44,9 @@ public class Address {
     @Size(min = 6, message = "Pincode must be atleast 6 characters")
     private String pincode;
 
-    @ToString.Exclude
-    @ManyToMany(mappedBy = "addresses")
-    private List<User> users = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public Address(String street, String buildingName, String city, String state, String country, String pincode) {
         this.street = street;
