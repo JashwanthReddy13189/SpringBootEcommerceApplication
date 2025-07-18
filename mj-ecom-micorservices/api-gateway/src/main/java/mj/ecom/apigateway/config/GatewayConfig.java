06228d7b3626045116694e1d5d420d7b7ef04f74
mr.jashwanthreddy <89520760+JashwanthReddy13189@users.noreply.github.com>
2025-07-18 12:17:03 +0530

    Added API Gateway microservice and monitoring config
    Introduced a new Spring Cloud Gateway-based API Gateway microservice with initial routing, JWT filter, logging, and configuration files. Updated Grafana and Prometheus configs to monitor the API Gateway, and added related log files.

---
+++ mj-ecom-micorservices/api-gateway/src/main/java/mj/ecom/apigateway/config/GatewayConfig.java
@@ -0,0 +1,31 @@
+package mj.ecom.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    // Adding this filter to not expose our original api mappings
+    // we are rewriting paths using custom configs
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r.path("/api/users/**")
+                        //.filters(f -> f.rewritePath("/users(?<segment>/?.*)", "/api/users${segment}")) // regex to hide api mappings
+                        .uri("lb://USER-SERVICE"))
+                .route("product-service", r -> r.path("/api/products/**")
+                        //.filters(f -> f.rewritePath("/products(?<segment>/?.*)", "/api/products${segment}"))
+                        .uri("lb://PRODUCT-SERVICE"))
+                .route("order-service", r -> r.path("/api/cart/**", "/api/orders/**")
+                        //.filters(f -> f.rewritePath("/(?<segment>.*)", "/api/${segment}"))
+                        .uri("lb://ORDER-SERVICE"))
+                .route("eureka-service", r -> r.path("/eureka/main")
+                        .filters(f -> f.rewritePath("/eureka/main", "/"))
+                        .uri("http://localhost:8761"))
+                .route("eureka-service-static", r -> r.path("/eureka/**")
+                        .uri("http://localhost:8761"))
+                .build();
+    }
+}
